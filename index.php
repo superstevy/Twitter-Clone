66098655c6eb2dcf97c4d589b41fdd9ec2d7aa29
Stevy <superstevy@yahoo.com>
2021-04-01 21:43:06 -0400

    Moved composer.json and index.php to main folder

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once("tweet/twitter/templates/twitter/index.html"); ?>
+
+<?php include_once("tweet/twitter/templates/twitter/load.html"); ?>
+
+<?php include_once("tweet/twitter/templates/twitter/delete.html"); ?>
+
+<?php include_once("tweet/twitter/templates/twitter/update.html"); ?>
