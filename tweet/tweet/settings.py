afc64c61298da2aaf967a9647d03069810e743bf
Stevy <superstevy@yahoo.com>
2021-04-02 15:08:57 -0400

    added STATICFILES_DIRS

--- tweet/tweet/settings.py
+++ tweet/tweet/settings.py
@@ -18,6 +18,7 @@ import cloudinary.api
 import django_heroku
 import dj_database_url
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +131,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
