69270b16b89920d70c8528be84ab85b3a37458cc
Stevy <superstevy@yahoo.com>
2021-04-02 18:23:18 -0400

    some commit

--- tweet/tweet/settings.py
+++ tweet/tweet/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
