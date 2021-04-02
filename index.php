9f484a6554d224b0e0c2cd996f98eb973396d1cb
Stevy <superstevy@yahoo.com>
2021-04-01 21:48:21 -0400

    little changes

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 <?php include_once("tweet/twitter/templates/twitter/index.html"); ?>
-
-<?php include_once("tweet/twitter/templates/twitter/load.html"); ?>
-
-<?php include_once("tweet/twitter/templates/twitter/delete.html"); ?>
-
-<?php include_once("tweet/twitter/templates/twitter/update.html"); ?>
